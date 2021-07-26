08f5aac133c0207e2bdc6c388fda066eea34d350
Your Name <you@example.com>
2021-07-26 15:07:31 +0500

    "eero add javaScript"

--- image-style/idex.js
+++ image-style/idex.js
@@ -7,9 +7,18 @@ window.addEventListener('DOMContentLoaded', function() {
         header.classList.toggle('sticy', window.scrollY > 0 );
     })
 
-    let bar = document.querySelector('.fa-bars');
+    let bar = document.querySelector('.fa-bars'),
+    menuX = document.querySelector('.fa-times');
     bar.addEventListener('click', function (){
         document.querySelector('nav > ul').classList.toggle('active');
+        bar.style.display = 'none';
+        menuX.style.display = 'block';
+
+        menuX.addEventListener('click', function() {
+          document.querySelector('nav ul').classList.toggle('no-active');
+          menuX.style.display = 'none';
+          bar.style.display = 'block';
+        })
     })
 
 
